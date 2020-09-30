6b5da6544a214de9e808dfefd6515455b0db5165
Jayson Obos <jayson.obos@lkg.com.au>
2020-10-01 02:24:51 +0800

    installed bootstrap via npm to avoid loading thru cdn. Tested working locally.

--- wp-content/themes/yourtheme/functions.php
+++ wp-content/themes/yourtheme/functions.php
@@ -120,6 +120,9 @@ function html5blank_styles()
 
     wp_register_style('html5blank', get_template_directory_uri() . '/style.css', array(), '1.0', 'all');
     wp_enqueue_style('html5blank'); // Enqueue it!
+
+    wp_register_style('bootstrap', '/node_modules/bootstrap/dist/css/bootstrap.min.css');
+    wp_enqueue_style('bootstrap'); // Enqueue it!
 }
 
 // Register HTML5 Blank Navigation
